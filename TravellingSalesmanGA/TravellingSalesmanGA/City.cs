9887d9029a25186e7a7f5fe0b0fefe7d6248c670
Jack Anderson <jack_120@hotmail.co.uk>
2016-10-09 20:23:17 +0100

    created city class

---
+++ TravellingSalesmanGA/TravellingSalesmanGA/City.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravellingSalesmanGA
+{
+    class City
+    {
+        private int _x, _y;
+
+        public City()
+        {
+
+        }
+
+        public City(int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public int X
+        {
+            get { return _x; }
+        }
+
+        public int Y
+        {
+            get { return _y;  }
+        }
+    }
+}
