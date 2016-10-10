57f39e01c5e0c9164f8962e8ee2b6b2721398239
Jack Anderson <jack_120@hotmail.co.uk>
2016-10-10 08:31:56 +0100

    basic tour class

--- TravellingSalesmanGA/TravellingSalesmanGA/Tour.cs
+++ TravellingSalesmanGA/TravellingSalesmanGA/Tour.cs
@@ -8,5 +8,33 @@ namespace TravellingSalesmanGA
 {
     class Tour
     {
+        //Holds each traversal between cities
+        private List<City> _tour = new List<City>();
+
+        private double _fitness = 0;
+        private int _distance = 0;
+
+        public Tour(int numCities)
+        {
+            for(int i = 0; i < numCities; i++)
+            {
+                _tour.Add(null);
+            }
+        }
+
+        public Tour(List<City> tour)
+        {
+            _tour = tour;
+        }
+
+        public void setCity(int index, City city)
+        {
+            _tour[index] = city;
+        }
+
+        public City getCity(int index)
+        {
+            return _tour[index];
+        }
     }
 }
